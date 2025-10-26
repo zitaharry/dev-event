fda8aa106d7a23f0a7515aac88a0b497c987fdf7
zitaharry <harrymuigua@gmail.com>
2025-10-26 18:48:53 +0300

    Implement Homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+import EventCard from "@/components/EventCard";
+import ExploreBtn from "@/components/ExploreBtn";
+import { events } from "@/lib/constants";
 import React from "react";
 
 const Page = () => {
@@ -9,6 +12,22 @@ const Page = () => {
 			<p className="text-center mt-5">
 				Hackathons, Meetups, and Conferences, All in One Place
 			</p>
+
+			<ExploreBtn />
+
+			<div className="mt-20 space-y-7">
+				<h3>Featured Events</h3>
+
+				<ul className="events">
+					{events &&
+						events.length > 0 &&
+						events.map((event: IEvent) => (
+							<li key={event.title} className="list-none">
+								<EventCard {...event} />
+							</li>
+						))}
+				</ul>
+			</div>
 		</section>
 	);
 };
