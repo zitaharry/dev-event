a04bc38fb0e4f7904e49429ab5fdb8bc5c8433c1
zitaharry <harrymuigua@gmail.com>
2025-10-30 19:22:59 +0300

    Fetch API routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,39 @@
 import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
-import { events } from "@/lib/constants";
 import React from "react";
+import { IEvent } from "@/database";
 
-const Page = () => {
-	return (
-		<section>
-			<h1 className="text-center">
-				The Hub for Every Dev <br /> Event You Can&apos;t Miss
-			</h1>
-			<p className="text-center mt-5">
-				Hackathons, Meetups, and Conferences, All in One Place
-			</p>
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-			<ExploreBtn />
+const Page = async () => {
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const { events } = await response.json();
 
-			<div className="mt-20 space-y-7">
-				<h3>Featured Events</h3>
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev <br /> Event You Can&apos;t Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences, All in One Place
+      </p>
 
-				<ul className="events">
-					{events &&
-						events.length > 0 &&
-						events.map((event: IEvent) => (
-							<li key={event.title} className="list-none">
-								<EventCard {...event} />
-							</li>
-						))}
-				</ul>
-			</div>
-		</section>
-	);
+      <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+
+        <ul className="events">
+          {events &&
+            events.length > 0 &&
+            events.map((event: IEvent) => (
+              <li key={event.title} className="list-none">
+                <EventCard {...event} />
+              </li>
+            ))}
+        </ul>
+      </div>
+    </section>
+  );
 };
 export default Page;
